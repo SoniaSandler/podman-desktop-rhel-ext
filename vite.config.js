1e25e7332091c7b64def3752044475b2300008cf
Sonia Sandler <ssandler@redhat.com>
2025-04-24 13:30:28 -0400

    chore: adjust vite file
    Signed-off-by: Sonia Sandler <ssandler@redhat.com>

--- vite.config.js
+++ vite.config.js
@@ -49,7 +49,6 @@ const config = {
       external: [
         '@podman-desktop/api',
         'ssh2',
-        '@crc-org/macadam.js',
         ...builtinModules.flatMap(p => [p, `node:${p}`]),
       ],
       output: {
